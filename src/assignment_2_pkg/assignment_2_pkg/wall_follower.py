b97cbb315afda4e162a9e9f650383c8154f383c0
Oystein_Falkeid <oystein.falkeid@NMBU.no>
2024-09-30 14:27:57 +0200

    assignment 2 lag 2 mitt i pid implementering

--- src/assignment_2_pkg/assignment_2_pkg/wall_follower.py
+++ src/assignment_2_pkg/assignment_2_pkg/wall_follower.py
@@ -20,6 +20,33 @@ class SimpleSubscriber(Node):
         # in this case, the subscriptor will be subscribed on /scan topic with a queue size of 10 messages.
         # use the LaserScan module for /scan topic
         # send the received info to the listener_callback method.
+        
+        # Gjennomsnittig avstand fra veggen
+        self.y0 = (30-17.8/2)/2 + 17.8/2 # ca. 19.45 cm
+        
+        # Konstante verdier
+        self.Ku = 0.1
+        self.Tu = 2.5
+        self.Kp = 0.6 * self.Ku
+        self.Ki = 1.2 * self.Ku
+        self.Kd = 0.075 * self.Ku * self.Tu
+        
+        self.I_max = 0.539999999999 # fra testen
+        
+        # Verdier som skal oppdateres
+        self.P = 0 # Propposjonal led
+        self.I = 0 # Integral led
+        self.D = 0 # Derrivasjons led
+        self.e_t = None # avvik i tiden t
+        
+        
+        # Første utkast
+        # self.Kp = 0.1
+        # self.Ki = 0.07
+        # self.integrator = 0
+        # self.offset = 0.2
+        
+    
         self.subscriber = self.create_subscription(
             LaserScan,
             '/scan',
@@ -29,35 +56,53 @@ class SimpleSubscriber(Node):
     
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
 
-    integrator = 0
-    
-    def listener_callback(self, laser_msg):
-        Kp = 0.1
-        Ki = 0.07
-        offset = 2.0
-        # denne koden løser oppgaven
-        distance_from_wall = min(laser_msg.ranges[:round(len(laser_msg.ranges)/2)])
-        self.get_logger().info(str(distance_from_wall))
+
+    def P_ledd(self):
+        self.P = self.e_t * self.Kp
         
-        twist_msg = Twist()
-        if min(laser_msg.ranges[:10]) < 0.25:
-            twist_msg.linear.x = 0.0
-            twist_msg.angular.z = -2.0
-            self.get_logger().info("obstacle")
-        else:
-            # define the linear x-axis velocity of /cmd_vel Topic parameter to 0.5
-            twist_msg.linear.x = 0.05
-            # define the angular z-axis velocity of /cmd_vel Topic parameter to 0.5
-            if distance_from_wall < 0.4:
-                self.integrator = self.integrator + (distance_from_wall*10.0-offset) * Ki
-                if 0.5 < self.integrator:
-                    self.integrator = 0.5
-                elif -0.5 > self.integrator:
-                    self.integrator = -0.5
-                    
-                twist_msg.angular.z = (distance_from_wall*10.0-offset) * Kp + self.integrator
-            elif distance_from_wall:
-                twist_msg.angular.z = (0.4*10.0-offset) * Kp
+    def I_ledd(self):
+        self.I = self.I + self.e_t * self.Ki
+        
+        # Begrens I når den er for stor/liten
+        if self.I_max < self.I:
+            self.I = self.I_max
+        elif -self.I_max > self.I:
+            self.I = -self.I_max
+            
+            
+    def D_ledd(self):
+        
+        # Mean filter
+        # current mean error
+        # previous mean error
+        # delta_t 
+        pass
+            
+        
+    
+        
+    def listener_callback(self, laser_msg):
+        
+        y = min(laser_msg.ranges[:round(len(laser_msg.ranges)/2)]) # distance from the wall
+        self.get_logger().info(str(y))
+        
+        twist_msg = Twist() # pådrag u
+        # if min(laser_msg.ranges[:10]) < 0.25:
+        #     twist_msg.linear.x = 0.0
+        #     twist_msg.angular.z = -2.0
+        #     self.get_logger().info("obstacle")
+        # else:
+        self.e_t = y - self.y0
+        # self.e_t = (distance_from_wall*10.0-self.offset)
+        # define the linear x-axis velocity of /cmd_vel Topic parameter to 0.5
+        twist_msg.linear.x = 0.05 
+        
+        # define the angular z-axis velocity of /cmd_vel Topic parameter to 0.5
+        if y < 0.3:
+            self.I_ledd()
+            self.P_ledd()
+            self.D_ledd()
+            twist_msg.angular.z = self.P + self.I *  + self.D
             
         # Publish the message to the Topic
         self.publisher_.publish(twist_msg)
