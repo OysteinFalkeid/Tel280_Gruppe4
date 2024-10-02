c40ceebdc24c3541f5a2ccc4271783fe548b99a7
Oystein_Falkeid <oystein.falkeid@NMBU.no>
2024-10-02 12:32:07 +0200

    implementering av D ledd klar for test

--- src/assignment_2_pkg/assignment_2_pkg/wall_follower.py
+++ src/assignment_2_pkg/assignment_2_pkg/wall_follower.py
@@ -1,6 +1,6 @@
 import rclpy
 # import the ROS2 python libraries
-from rclpy.node import Node
+from rclpy.node import Nodeygttt
 # import the LaserScan module from sensor_msgs interface
 from sensor_msgs.msg import LaserScan
 # import Quality of Service library, to set the correct profile and reliability to read sensor data.
@@ -19,10 +19,11 @@ class SimpleSubscriber(Node):
         # create the subscriber object
         # in this case, the subscriptor will be subscribed on /scan topic with a queue size of 10 messages.
         # use the LaserScan module for /scan topic
+        
         # send the received info to the listener_callback method.
         
-        # Gjennomsnittig avstand fra veggen
-        self.y0 = (30-17.8/2)/2 + 17.8/2 # ca. 19.45 cm
+        # Gjennomsnittlig avstand fra veggen
+        self.y0 = (30 - 17.8/2)/2 + 17.8/2 # ca. 19.45 cm
         
         # Konstante verdier
         self.Ku = 0.1
@@ -34,36 +35,35 @@ class SimpleSubscriber(Node):
         self.I_max = 0.539999999999 # fra testen
         
         # Verdier som skal oppdateres
-        self.P = 0 # Propposjonal led
-        self.I = 0 # Integral led
-        self.D = 0 # Derrivasjons led
-        self.e_t = None # avvik i tiden t
+        self.P = 0 # Propposjonal ledd
+        self.I = 0 # Integral ledd
+        self.D = 0 # Derrivasjons ledd
+        self.e_t = 0 # avvik i tiden t
         
         
-        # Første utkast
-        # self.Kp = 0.1
-        # self.Ki = 0.07
-        # self.integrator = 0
-        # self.offset = 0.2
+        self.e = [0, 0, 0] # de tre siste feilene for å beregne gjennomsnittlig feil
+        self.i = 0  # indeks for oppdatering av self.e
+        self.e_snitt_pre = 0 # forrige gjennomsnittlig feil
         
-    
+        
         self.subscriber = self.create_subscription(
             LaserScan,
             '/scan',
             self.listener_callback,
             QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))  # is the most used to read LaserScan data and some sensor data.
         
-    
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
 
 
     def P_ledd(self):
+        """Beregner proposjonal ledd."""
         self.P = self.e_t * self.Kp
         
     def I_ledd(self):
+        """Beregner integral ledd og begrenser det."""
         self.I = self.I + self.e_t * self.Ki
         
-        # Begrens I når den er for stor/liten
+        # Begrenser I når den er for stor/liten
         if self.I_max < self.I:
             self.I = self.I_max
         elif -self.I_max > self.I:
@@ -71,19 +71,19 @@ class SimpleSubscriber(Node):
             
             
     def D_ledd(self):
-        
-        # Mean filter
-        # current mean error
-        # previous mean error
-        # delta_t 
-        pass
-            
-        
+        """Beregner derivasjons ledd med gjennomsnittlig filter."""
+        self.e[self.i] = self.e_t
+        e_snitt = sum(self.e) / len(self.e)
+        self.D = (e_snitt - self.e_snitt_pre) / 0.09 * self.Kd  # laser_msg.scan_time er ca. 0.09 sekunder fra testen
+        self.e_snitt_pre = e_snitt
+        self.i += 1
+        if self.i > 2:
+            self.i = 0
     
         
     def listener_callback(self, laser_msg):
         
-        y = min(laser_msg.ranges[:round(len(laser_msg.ranges)/2)]) # distance from the wall
+        y = min(laser_msg.ranges[:round(len(laser_msg.ranges)/2)]) # målte avstand fra veggen
         self.get_logger().info(str(y))
         
         twist_msg = Twist() # pådrag u
@@ -95,21 +95,20 @@ class SimpleSubscriber(Node):
         self.e_t = y - self.y0
         # self.e_t = (distance_from_wall*10.0-self.offset)
         # define the linear x-axis velocity of /cmd_vel Topic parameter to 0.5
-        twist_msg.linear.x = 0.05 
+        twist_msg.linear.x = 0.05
         
-        # define the angular z-axis velocity of /cmd_vel Topic parameter to 0.5
         if y < 0.3:
             self.I_ledd()
             self.P_ledd()
             self.D_ledd()
-            twist_msg.angular.z = self.P + self.I *  + self.D
+            twist_msg.angular.z = self.P + self.I  + self.D
             
         # Publish the message to the Topic
         self.publisher_.publish(twist_msg)
         # Display the message on the console
         self.get_logger().info('Publishing: "%s"' % twist_msg)
+
         
-        #pass
         
 
 
