fd02ff57029e0a3f6c92b1c3f9fc3776784cf1e0
Oystein_Falkeid <oystein.falkeid@NMBU.no>
2024-10-16 13:15:38 +0200

    ferdig assignment 3

--- src/assignment_3_pkg/launch/assignment_3_pkg.launch.py
+++ src/assignment_3_pkg/launch/assignment_3_pkg.launch.py
@@ -1,1 +1,36 @@
-from launch.actions import IncludeLaunchDescription
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import time
+
+
+def generate_launch_description():
+    # Find paths to packages
+    turtlebot_bringup_directory = get_package_share_directory('turtlebot3_bringup')
+    cartographer_slam_directory = get_package_share_directory('cartographer_slam')
+    assignment_2_directory = get_package_share_directory('assignment_2_pkg')
+    
+    # Create launch descriptions
+    bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([turtlebot_bringup_directory, '/launch/robot.launch.py'])
+        )
+    
+    cartographer_slam_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([cartographer_slam_directory, '/launch/cartographer.launch.py'])
+        )
+    
+    assignment_2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([assignment_2_directory, '/assignment_2_launch_file.launch.py'])
+        )
+
+    return LaunchDescription([
+        bringup_launch,
+        TimerAction(
+            actions = [
+                cartographer_slam_launch,
+                assignment_2_launch], period = 5.0)
+        ])
+    
+    
+    
--- src/cartographer_slam/config/cartographer.lua
+++ src/cartographer_slam/config/cartographer.lua
@@ -30,9 +30,9 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.min_range = 0.12
-TRAJECTORY_BUILDER_2D.max_range = 3.5
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.0
+TRAJECTORY_BUILDER_2D.min_range = 0.17
+TRAJECTORY_BUILDER_2D.max_range = 2.5
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.0
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true 
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
