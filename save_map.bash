81536760709205ea5bd0cb22b34cc26a90446336
Oystein_Falkeid <oystein.falkeid@NMBU.no>
2024-11-06 14:25:12 +0100

    lokalisering og path planing fungerer

--- save_map.bash
+++ save_map.bash
@@ -1,6 +1,6 @@
 while :
 do
-   cd ~/ros2_ws/map
-   ros2 run nav2_map_server map_saver_cli -f assignment_3_map
+   cd ~/ros2_ws/home/Gruppe4/ros2_ws/install/map_server/share/map_server/config/
+   ros2 run nav2_map_server map_saver_cli -f turtlebot_area
    sleep 0.5
 done
